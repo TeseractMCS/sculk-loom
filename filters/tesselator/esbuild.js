6d6778d1a64f12871419df7e835d7ee4af7f5aa0
CibNumeritos <imcib2498.212244.2808@gmail.com>
2024-09-11 18:32:01 -0500

    fix: privateFields code missing (forced injection)

--- filters/tesselator/esbuild.js
+++ filters/tesselator/esbuild.js
@@ -51,10 +51,22 @@ build({
             cwd: process.cwd(),
         }),
     ],
-    treeShaking: false
+    treeShaking: true
 }).then(() => {
     const code = readFileSync("BP/scripts/index.js").toString();
-    writeFileSync("BP/scripts/index.js", `var __decorate = function(decorators, target, key, desc) {
+    writeFileSync("BP/scripts/index.js", `
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var __decorate = function(decorators, target, key, desc) {
   var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
   if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
   else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
